b74c0521645ff61537a1a4b9c01a33f1d6401b13
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-11-12 16:27:17 +0100

    inlogg

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// require("dotenv").config();
-// const express = require("express");
-// const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
 // const auth = require("./middlewares/auth");
 // const fs = require("fs");
 const port = 3000;
@@ -10,11 +10,17 @@ let a = 0,
 let totalA = 0;
 let totalB = 0;
 let totalVotes = 0;
-const qId = "sefwqrqergerg";
+const loginPassword = "666";
 
-// const app = express();
+const users = [
+    {
+        name: "Oscar",
+        password: loginPassword,
+    },
+];
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
 const io = require("socket.io")(port, {
     cors: {
@@ -24,13 +30,13 @@ const io = require("socket.io")(port, {
 
 io.on("connection", (socket) => {
     socket.on("sendQuestion", (questionString, codeString) => {
-        reset();
+        //reset();
         io.emit("recieveQuestion", questionString, codeString);
     });
 
     function reset() {
         (a = 0), (b = 0), (totalA = 0), (totalB = 0), (totalVotes = 0);
-        io.emit("reset");
+        //io.emit("reset");
     }
 
     socket.on("reset", () => {
@@ -49,5 +55,19 @@ io.on("connection", (socket) => {
         }
         totalVotes += 1;
         io.emit("recieveVote", totalA, totalB);
+    });
+
+    socket.on("login", (password) => {
+        const accessToken = jwt.sign(users[0], process.env.SECRET_KEY);
+        const decoded = jwt.verify(accessToken, process.env.SECRET_KEY);
+        if (decoded.password === loginPassword) {
+            io.emit("token", accessToken);
+        } else io.emit("token", 404);
+    });
+    socket.on("checkToken", (token) => {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        if (decoded.password === loginPassword) {
+            socket.emit("confirmToken", true, users[0].name);
+        }
     });
 });
